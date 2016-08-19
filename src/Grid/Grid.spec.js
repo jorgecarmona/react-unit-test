717b0cb0779f04a3ebd4ba11329af04a222fd8ff
jorgecarmona <jcarmona@predixionsoftware.com>
2016-08-19 12:56:11 -0400

    Added describe blocks as groups for clarity

--- src/Grid/Grid.spec.js
+++ src/Grid/Grid.spec.js
@@ -5,59 +5,65 @@ import { shallow } from 'enzyme';
 import Grid from './Grid';
 
 describe('<Grid />', () => {
-	let wrapper = null;
+  let wrapper = null;
 
-	beforeEach(() => {
-		// Creates a new instance of Grid component before each test
-		wrapper = shallow(<Grid />);		
-	});
+  beforeEach(() => {
+    // Creates a new instance of Grid component before each test
+    wrapper = shallow(<Grid />);    
+  });
 
-	it('renders without exploding', () => {
-		expect(wrapper.length).to.equal(1);
-	});
+  it('renders without exploding', () => {
+    expect(wrapper.length).to.equal(1);
+  });
 
-	it('should have a rows Immutable List in state', () => {	
-		expect(Immutable.List.isList(wrapper.state().rows)).to.be.true;
-	});	
+  describe('Component state', () => {
+    it('should have a rows Immutable List in state', () => {  
+      expect(Immutable.List.isList(wrapper.state().rows)).to.be.true;
+    }); 
 
-	it('should have an "Add new row" button', () => {
-		expect(wrapper.find('.add').length).to.equal(1);
-	});
+    it('should keep count of number of rows', () => {
+      wrapper.find('.add').simulate('click');
 
-	it('should have a delete button on each row', () => {
-		expect(wrapper.state().rows.size).to.equal(0);
+      expect(+wrapper.find('.row-count').text()).to.equal(wrapper.state().rows.size);
+    });
+  });
 
-		wrapper.find('.add').simulate('click');
-		expect(wrapper.find('.row').childAt(1).hasClass('delete')).to.be.true;	
-	});
-	
-	it('should add a new row when plus button is clicked', () => {
-		wrapper.find('.add').simulate('click');		
+  describe('Add row feature', () => {
+    it('should have an "Add new row" button', () => {
+      expect(wrapper.find('.add').length).to.equal(1);
+    });
+  
+    it('should add a new row when plus button is clicked', () => {
+      wrapper.find('.add').simulate('click');   
 
-		expect(wrapper.state().rows.size).to.equal(1);
-	});
-	
-	it('should remove row when delete button is clicked', () => {
-		// add row so delete button is present
-		wrapper.find('.add').simulate('click');
-		// state now has 1
-		expect(wrapper.state().rows.size).to.equal(1);
+      expect(wrapper.state().rows.size).to.equal(1);
+    });
+  });
 
-		// remove row
-		wrapper.find('.row').childAt(1).simulate('click');
-		// state is now empty
-		expect(wrapper.state().rows.size).to.equal(0);
-	});
+  describe('Remove row feature', () => {
+    it('should have a delete button on each row', () => {
+      expect(wrapper.state().rows.size).to.equal(0);
 
-	it('should keep count of number of rows', () => {
-		wrapper.find('.add').simulate('click');
+      wrapper.find('.add').simulate('click');
+      expect(wrapper.find('.row').childAt(1).hasClass('delete')).to.be.true;  
+    });
+    
+    it('should remove row when delete button is clicked', () => {
+      // add row so delete button is present
+      wrapper.find('.add').simulate('click');
+      // state now has 1
+      expect(wrapper.state().rows.size).to.equal(1);
 
-		expect(+wrapper.find('.row-count').text()).to.equal(wrapper.state().rows.size);
-	});
+      // remove row
+      wrapper.find('.row').childAt(1).simulate('click');
+      // state is now empty
+      expect(wrapper.state().rows.size).to.equal(0);
+    });
 
-	it('should remove all rews when "Delete all rows" button is clicked', () => {
-		wrapper.find('.remove-all').simulate('click');
+    it('should remove all rews when "Delete all rows" button is clicked', () => {
+      wrapper.find('.remove-all').simulate('click');
 
-		expect(wrapper.state().rows.size).to.equal(0);
-	});
+      expect(wrapper.state().rows.size).to.equal(0);
+    });
+  });
 })
